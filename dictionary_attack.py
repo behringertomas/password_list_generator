4bca2ffa4378585f84c126fa104ab46ed8e5666a
tbehringer <tomas.behringer@mercadolibre.com>
2020-04-23 20:26:21 -0300

    dictionary_attack: le doy la forma al main

---
+++ dictionary_attack.py
@@ -0,0 +1,28 @@
+import os
+from service import *
+
+
+def main():
+    os.system('clear')
+    banner_menu()
+    menu()
+    print(menu())
+
+    print("{0}Ingrese OK para finalizar de llenar la información.".format(DEFAULT))
+    try:
+        option = get_user_input()
+        if str(option).lower() != "ok":
+            option = int(option)
+            add_data(option)
+            # main()
+        # else:
+    except ValueError:
+        main()
+    except KeyboardInterrupt as e:
+        print(e)
+    except Exception as e:
+        print(e)
+
+
+if __name__ == '__main__':
+    main()
